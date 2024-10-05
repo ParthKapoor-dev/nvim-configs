baf20c1e3f9398b4b349e1b9e1097fceffb94e67
Parth Kapoor <pkapoor_be22@thapar.edu>
2024-10-06 02:37:04 +0530

    Minor updates

--- lua/parth/plugins/formatting.lua
+++ lua/parth/plugins/formatting.lua
@@ -20,6 +20,8 @@ return {
             liquid = { "prettier" },
             lua = { "stylua" },
             python = { "isort", "black" },
+            c = { "clang_format" },
+            rust = { "rustfmt" },
          },
          format_on_save = {
             lsp_fallback = true,
